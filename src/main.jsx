bfae602246188b096b014327cc3df3012e3d8a67
mdhmpy62dp-dot <mdhmpy62dp@privaterelay.appleid.com>
2026-09-02 20:47:37 +0800

    Replace App component content with Home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from './pages/Home'
 import './app.css'
 
 function App() {
-  return (
-    <div className="app">
-      <h1>PayGate</h1>
-      <p>商户中心正在初始化</p>
-    </div>
-  )
+  return <Home />
 }
 
 createRoot(document.getElementById('root')).render(
